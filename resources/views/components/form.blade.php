b0c0148ef14ffb73e48c28bc9e20d810d4369ba0
Johan <johan@johanvanhelden.com>
2020-09-11 13:47:33 +0200

    add id support to forms

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="{{ $action }}" class="{{ $class ?? '' }}">
+<form id="{{ $id ?? '' }}" method="POST" action="{{ $action }}" class="{{ $class ?? '' }}">
     <input name="_method" type="hidden" value="{{ $method ?? 'POST' }}">
     @csrf
 
