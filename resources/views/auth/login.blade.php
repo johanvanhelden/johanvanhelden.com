8cd6547045df4b3ad7db1e699e560c6742153346
Johan <johan@johanvanhelden.com>
2020-09-11 08:38:35 +0200

    cleaning

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,19 @@
 
 @section('card-header', __('page-auth.login.headings.form'))
 @section('card-content')
+    <x-form action="{{ route('login') }}">
+        <x-forms.inputs.input type="email" name="email" label="{{ __('user.attributes.email') }}" />
+        <x-forms.inputs.input type="password" name="password" label="{{ __('user.attributes.password') }}" />
+        <x-forms.inputs.checkbox name="remember" label="{{ __('page-auth.login.remember') }}" />
 
-<x-form action="{{ route('login') }}">
-    <x-forms.inputs.input type="email" name="email" label="{{ __('user.attributes.email') }}" />
-    <x-forms.inputs.input type="password" name="password" label="{{ __('user.attributes.password') }}" />
-    <x-forms.inputs.checkbox name="remember" label="{{ __('page-auth.login.remember') }}" />
+        <div class="flex items-center justify-between">
+            <button type="submit" class="button button--primary" dusk="login-button">
+                {{ __('action.login') }}
+            </button>
 
-    <div class="flex items-center justify-between">
-        <button type="submit" class="button button--primary" dusk="login-button">
-            {{ __('action.login') }}
-        </button>
-
-        <a href="{{ route('password.request') }}" class="link link--primary">
-            {{ __('action.forgot_password') }}
-        </a>
-    </div>
-</x-form>
+            <a href="{{ route('password.request') }}" class="link link--primary">
+                {{ __('action.forgot_password') }}
+            </a>
+        </div>
+    </x-form>
 @endsection
