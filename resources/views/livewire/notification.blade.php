340aa63183134d356d39128a98422e0316593fb1
Johan van Helden <johan@johanvanhelden.com>
2022-01-06 17:26:14 +0100

    upgrade composer & npm packages + alpine 3.0 changes and tailwind 3.0 changes

--- resources/views/livewire/notification.blade.php
+++ resources/views/livewire/notification.blade.php
@@ -1,9 +1,9 @@
 <div class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end | z-50">
     <div
-        x-init="setTimeout(() => open = true, 50)"
         x-data="{ open: false }"
+        x-init="setTimeout(() => open = true, 50)"
         x-show="open"
-        x-transition:enter="transform ease-out duration-300 transition"
+        x-transition:enter="ease-out duration-300"
         x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
         x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
         x-transition:leave="transition ease-in duration-100"
@@ -14,7 +14,7 @@
         <div class="rounded-lg ring overflow-hidden">
             <div class="p-4">
                 <div class="flex items-start">
-                    <div class="flex-shrink-0">
+                    <div class="shrink">
                         @if ($type === 'info')
                             <i class="fas fa-exclamation-circle text-blue-600"></i>
                         @elseif ($type === 'error')
@@ -29,7 +29,7 @@
                             <p class="mt-1 text-sm leading-5 text-gray-600">{{ $description }}</p>
                         @endif
                     </div>
-                    <div class="ml-4 flex-shrink-0 flex">
+                    <div class="ml-4 shrink flex">
                         <button x-on:click="open = false; $wire.dismiss()" class="inline-flex text-gray-400 focus:outline-none focus:text-gray-600 hover:text-gray-600 transition ease-in-out duration-150">
                             <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
